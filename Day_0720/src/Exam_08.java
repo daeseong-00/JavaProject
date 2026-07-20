f22f6860c403a8223eca061334adb93a95fd9e58
vseot <vseot@172.30.1.100>
2026-07-20 16:03:54 +0900

    2026.07.20 오후연습

---
+++ Day_0720/src/Exam_08.java
@@ -0,0 +1,14 @@
+class Point{
+	int a;
+	int b;
+	int c=5;//명시적 초기화
+	static int d;//정적(클래스변수)
+}
+public class Exam_08 {
+	public static void main(String[] args) {
+		Point p1 = new Point();
+		Point p2 = new Point();
+
+	}
+
+}
