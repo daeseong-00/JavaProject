d829e4a6746c317618d3e53996399d771b218972
vseot <vseot@172.30.1.87>
2026-07-23 17:47:04 +0900

    2026.07.23 오후연습

---
+++ Day_0723/src/com/mnu/oracle/OracleExam_04.java
@@ -0,0 +1,33 @@
+package com.mnu.oracle;
+import java.sql.*;//이것도 처음에 임포트하고 시작하는게 편함
+
+public class OracleExam_04 {
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null; //이 셋은 필수
+		try {
+			//1. 드라이버 로딩
+			Class.forName("oracle.jdbc.OracleDriver");
+			//2. 커넥션 연결
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "c##test", "1234");
+			//3. 명령문 수행
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT ENO, ENAME, HIREDATE, SALARY FROM EMP WHERE DNO=10\r\n"
+					+ "ORDER BY SALARY DESC");
+			
+			//4. 출력
+			while(rs.next()) {
+				System.out.print(rs.getInt("eno") + "\t");
+				System.out.print(rs.getString("ename") + "\t");
+				System.out.print(rs.getString("hiredate") + "\t");
+				System.out.print(rs.getInt("salary") + "\n");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
